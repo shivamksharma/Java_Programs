1aedb91ee3bdd524ea483bae9f7bd29110fc4999
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2019-12-14 17:53:13 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+// With Hello World Program in any programming language isn't a good start, I think.
+Here is a thing that in this repository I use GitHub Program as every java class(you can change it if you want to modify the code)
+
+/*Hello World Normal People*/
+
+class GitHubProgram {
+
+  public staic void main(String [] args) {
+  
+    System.out.println("Welcome to GitHub JavaPrograms");
+    }
+ }
+ 
+
+------------------------------------------------------------------------------------------------------------------------------
+
+
+/*Hello World Second Type*/
+
+public class GitHub Programs  {
+    
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      String inputString = scan.nextLine();
+      scan.close();
+      
+      System.out,println("Welcome to GitHub JavaPrograms");
+      System.out.println(inputString);
+    }
+}
