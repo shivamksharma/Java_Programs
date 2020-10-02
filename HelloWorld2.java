e7ea30813f38ff9f0947f8b6481319eac91c75d0
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2020-10-02 18:15:42 +0530

    Update HelloWorld2.java

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,12 +1,19 @@
+// The Best Way to Start any Programming Language is Create a Simple Program to Understand the Basics.
+// HelloWorld! is the best&easy Program and commonly use such programs as basic sanity test that Output/Print
+// In Every Program, Class is (ProgrammingLanguageName)GithubPrograms
+// If You Download This Repository, You've to change the Class Name as the Program Name!
+
+
+
 import java.util.Scanner;
 
-public class helloworld2 {
-	public static void main(String [] args) {
-		Scanner scan = new Scanner(System.in);
-		String inputString = scan.nextLine();
-		scan.close();
+	public class JavaGithubPrograms {
+		public static void main(String [] args) {
+			Scanner scan = new Scanner(System.in);
+			String inputString = scan.nextLine();
+			scan.close();
 
-	System.out.println("Welcome to Github Repository");
-	System.out.println(inputString);
+		System.out.println("Welcome to Java Programs Github Repository");
+		System.out.println(inputString);
+		}
 	}
-}
