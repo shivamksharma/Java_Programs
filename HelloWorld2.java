f2cb36a71f3f92e1adff0777638f471a61a024d2
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2020-10-02 18:13:52 +0530

    Create HelloWorld2.java

---
+++ HelloWorld2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class helloworld2 {
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+		String inputString = scan.nextLine();
+		scan.close();
+
+	System.out.println("Welcome to Github Repository");
+	System.out.println(inputString);
+	}
+}
