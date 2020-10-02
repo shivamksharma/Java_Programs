54f5ff86be84cfb9667c52058a78a8f6320f687e
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2020-10-02 17:53:32 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,30 +1,11 @@
-// With Hello World Program in any programming language isn't a good start, I think.
-Here is a thing that in this repository I use GitHub Program as every java class(you can change it if you want to modify the code)
+// The Best Way to Start any Programming Language is Create a Simple Program to Understand the Basics.
+// HelloWorld! is the best&easy Program and commonly use such programs as basic sanity test that Output/Print
+// In Every Program, Class is (ProgrammingLanguageName)GithubPrograms
+// If You Download This Repository, You've to change the Class Name as the Program Name!
 
-/*Hello World Normal People*/
+class JavaGithubPrograms
+	public static void main(String [] args) {
+		System.out.println("Welcome To Java Programs Repository");
+		}
+	}
 
-class GitHubProgram {
-
-  public staic void main(String [] args) {
-  
-    System.out.println("Welcome to GitHub JavaPrograms");
-    }
- }
- 
-
-------------------------------------------------------------------------------------------------------------------------------
-
-
-/*Hello World Second Type*/
-
-public class GitHub Programs  {
-    
-    public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      String inputString = scan.nextLine();
-      scan.close();
-      
-      System.out,println("Welcome to GitHub JavaPrograms");
-      System.out.println(inputString);
-    }
-}
